a46975f861250c1e699a1241e4c68a203bce81a5
wenlianghuang <wenliangmatt@gmail.com>
2023-12-12 22:58:28 +0800

    record 1212

--- scraper.go
+++ scraper.go
@@ -43,26 +43,63 @@ func GetHttpHtmlContent(url string, selector string, sel interface{}) (string, e
 	return htmlContent, nil
 }
 
-func GetSpecialData(htmlContent string, selector string) (string, error) {
+func GetSpecialData(htmlContent string, selector string) ([][]string, error) {
 	dom, err := goquery.NewDocumentFromReader(strings.NewReader(htmlContent))
 	if err != nil {
 		log.Fatal(err)
-		return "", err
+		return nil, err
 	}
-	var str string
+
+	var results [][]string
+
+	// 使用選擇器尋找所有匹配的元素
 	dom.Find(selector).Each(func(i int, selection *goquery.Selection) {
-		str = selection.Text()
+		// 在每個匹配的元素中查找"td"標籤並提取文本
+		/*
+			selection.Find("td").Each(func(j int, trSelection *goquery.Selection) {
+				// 将每个"tr"标签的文本追加到结果数组
+				results = append(results, trSelection.Text())
+			})
+		*/
+
+		//tr下的第一個td
+		/*
+			firstTd := selection.Find("td:nth-child(1)")
+			results = append(results, firstTd.Text())
+			//tr下的第二個td
+			secondTd := selection.Find("td:nth-child(2)")
+			results = append(results, secondTd.Text())
+			//tr下的最後一個
+			lasttd := selection.Find("td:last-child")
+			results = append(results, lasttd.Text())
+		*/
+		selection.Find("tr").Each(func(j int, trSelection *goquery.Selection) {
+			var row []string
+			trSelection.Find("td").Each(func(k int, tdSelection *goquery.Selection) {
+				row = append(row, tdSelection.Text())
+			})
+
+			results = append(results, row)
+		})
 	})
-	return str, nil
+
+	return results, nil
 }
 func main() {
 	//selector := "body > div > div.banner > div.swiper-container-place > div > div.swiper-slide.swiper-slide-0.swiper-slide-visible.swiper-slide-active > a.item.item-big > div.item-bottom"
-	selector := "#MainContent > ul > li > article > div > div > div.tb-out > table > tbody > tr:nth-child(2)"
+	//selector := "#MainContent > ul > li > article > div > div > div.tb-out > table > tbody > tr:nth-child(1) > th:nth-child(1)"
+	selector := "#MainContent > ul > li > article > div > div > div.tb-out > table > tbody"
+
 	param := `document.querySelector("body")`
-	url := "https://www.cmoney.tw/finance/6414/f00027"
+	url := "https://www.cmoney.tw/finance/3293/f00027"
 	html, _ := GetHttpHtmlContent(url, selector, param)
 	//res, _ := GetSpecialData(html, ".tb-out")
 	res, _ := GetSpecialData(html, selector)
-	fmt.Println(res)
+	for i, row := range res {
+		if len(row) == 0 {
+			continue
+		}
+		fmt.Printf("Row %d: 年度:%v, 現金股利:%v,股票股利合計:%v, 股利合計:%v\n", i+1, row[0], row[1], row[len(row)-2], row[len(row)-1])
+	}
 	return
 }
